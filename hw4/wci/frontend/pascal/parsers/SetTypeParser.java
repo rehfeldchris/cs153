c62ea20947e2eb7dc39b3caad3d2885ebe99adb4
rehfeldchris <rehfeldchris@gmail.com>
2013-10-02 22:22:33 -0700

    Add compile time type checking for set type declarations. For example, it will catch stuff like 'set of real' or 'set of myRecord' as an error.

--- hw4/wci/frontend/pascal/parsers/SetTypeParser.java
+++ hw4/wci/frontend/pascal/parsers/SetTypeParser.java
@@ -1,18 +1,36 @@
 package wci.frontend.pascal.parsers;
 
+import static wci.frontend.pascal.PascalErrorCode.INVALID_SET_ELEMENT_TYPE;
 import static wci.frontend.pascal.PascalErrorCode.MISSING_OF;
+import static wci.frontend.pascal.PascalTokenType.IDENTIFIER;
+import static wci.frontend.pascal.PascalTokenType.INTEGER;
+import static wci.frontend.pascal.PascalTokenType.LEFT_PAREN;
+import static wci.frontend.pascal.PascalTokenType.MINUS;
 import static wci.frontend.pascal.PascalTokenType.OF;
+import static wci.frontend.pascal.PascalTokenType.PLUS;
+import static wci.frontend.pascal.PascalTokenType.REAL;
 import static wci.frontend.pascal.PascalTokenType.SEMICOLON;
+import static wci.frontend.pascal.PascalTokenType.STRING;
 import static wci.intermediate.typeimpl.TypeFormImpl.SET;
+import static wci.intermediate.typeimpl.TypeFormImpl.SCALAR;
+import static wci.intermediate.typeimpl.TypeFormImpl.ENUMERATION;
 import static wci.intermediate.typeimpl.TypeKeyImpl.SET_ELEMENT_TYPE;
+import static wci.intermediate.symtabimpl.Predefined.booleanType;
+import static wci.intermediate.symtabimpl.Predefined.integerType;
+import static wci.intermediate.symtabimpl.Predefined.charType;
 
+import java.util.Arrays;
 import java.util.EnumSet;
+import java.util.List;
 
 import wci.frontend.Token;
 import wci.frontend.pascal.PascalParserTD;
 import wci.frontend.pascal.PascalTokenType;
 import wci.intermediate.TypeFactory;
+import wci.intermediate.TypeForm;
 import wci.intermediate.TypeSpec;
+import wci.intermediate.typeimpl.TypeFormImpl;
+import wci.intermediate.typeimpl.TypeSpecImpl;
 
 /**
  * <h1>SetTypeParser</h1>
@@ -40,6 +58,12 @@ class SetTypeParser extends TypeSpecificationParser
        OF_SET.add(OF);
        OF_SET.add(SEMICOLON);
    }
+   
+   
+   // Synchronization set for OF.
+   private static final List<TypeSpec> VALID_SET_ELEMENT_TYPES =
+		   Arrays.asList(integerType, booleanType, charType);
+   
 
    /**
     * Parse a Pascal set type specification.
@@ -65,7 +89,32 @@ class SetTypeParser extends TypeSpecificationParser
        SimpleTypeParser simpleTypeParser = new SimpleTypeParser(this);
        
        // Parse the element type.
-       setType.setAttribute(SET_ELEMENT_TYPE, simpleTypeParser.parse(token));
+       TypeSpec typeSpec = simpleTypeParser.parse(token);
+       
+       //check the form and type to make sure theyre valid for set element
+       switch ((TypeFormImpl) typeSpec.getForm()) {
+       case SCALAR:
+    	   //intentional fall through
+       case SUBRANGE:
+    	   //only certain scalars are legal. eg, "set of real" isnt allowed, but real is a scalar.
+    	   //we check the baseType() because "set of 1..5" is legal, and we need to check the type of the subrange.
+    	   //we always use the baseType because baseType() of a scalar is itself.
+           if (!VALID_SET_ELEMENT_TYPES.contains(typeSpec.baseType())) {
+        	   errorHandler.flag(token, INVALID_SET_ELEMENT_TYPE, this);
+           }
+           break;
+           
+       case ENUMERATION:
+    	   // always ok
+           break;
+           
+       default:
+    	   //any other type of set is forbidden
+    	   errorHandler.flag(token, INVALID_SET_ELEMENT_TYPE, this);
+       }
+
+       
+       setType.setAttribute(SET_ELEMENT_TYPE, typeSpec);
 
        return setType;
    }
